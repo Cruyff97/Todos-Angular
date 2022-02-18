7562ceb582d612e42b0a94bf33d3dbd337d20abf
cruyff97 <pelmat97@gmail.com>
2022-02-18 14:00:37 +0100

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,8 @@ import { SharedDatasService } from './service/shared-datas.service';
 })
 export class AppComponent implements OnInit {
  newTasks?: any;
-  tasks: Task[]=[{
-    task: 'Learn Angular',
-    deadline: "03-04-2030",
-    description: "I had to learn Angular in depth"
-  },
-  {
-    task: 'Learn React',
-    deadline: "03-04-2040",
-    description: "I had to learn React in depth"
-  }]
+
   title = 'Todo';
-  shared?: Subscription
   ngOnInit(): void {
   
   }
