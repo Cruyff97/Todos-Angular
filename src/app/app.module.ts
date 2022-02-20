3bc7f88e234a8bd3d62f7c6c311108198f081261
cruyff97 <pelmat97@gmail.com>
2022-02-20 14:00:51 +0100

    add material modules in a separate file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
+import { MaterialModule } from './modules/material/material.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatTabsModule} from '@angular/material/tabs';
 import { TabsComponent } from './tabs/tabs.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,11 +8,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TodoInputComponent } from './todo-input/todo-input.component';
 import { TasksComponent } from './tasks/tasks.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatIconModule} from '@angular/material/icon';
 import { CompletedComponent } from './completed/completed.component';
 
 @NgModule({
@@ -28,14 +23,9 @@ import { CompletedComponent } from './completed/completed.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    MatTabsModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,
     ReactiveFormsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatExpansionModule,
-    MatIconModule
+  MaterialModule
    
   ],
   providers: [],
